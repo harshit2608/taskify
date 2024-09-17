2070832821fbb15a80dab45b3dd069ac9944f78d
harshit2608 <harshitrock111@gmail.com>
2024-09-18 01:06:24 +0530

    Add organization support | Authentication

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,58 @@
+import Logo from '@/components/logo';
+import { Button } from '@/components/ui/button';
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
+import { Plus } from 'lucide-react';
+
+const Navbar = () => {
+  return (
+    <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+        <Button
+          size="sm"
+          variant="primary"
+          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+        >
+          Create
+        </Button>
+        <Button
+          size="sm"
+          variant="primary"
+          className="rounded-sm block md:hidden"
+        >
+          <Plus />
+        </Button>
+      </div>
+      <div className="flex ml-auto items-center gap-x-2">
+        <OrganizationSwitcher
+          afterCreateOrganizationUrl="/organization/:id"
+          afterSelectOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              },
+            },
+          }}
+        />
+        <UserButton
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              },
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,7 @@
+import { OrganizationSwitcher } from '@clerk/nextjs';
+
+const OrganizationId = () => {
+  return <OrganizationSwitcher />;
+};
+
+export default OrganizationId;
